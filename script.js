24fa51ea0ced73b07d8de338981f425b7df5b508
aminooosm <240103131@sdu.edu.kz>
2026-03-14 15:34:12 +0500

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// Таймер до ифтара
 const iftarDate = new Date("March 17, 2026 18:30:00").getTime();
 
-setInterval(function() {
-    const now = new Date().getTime();
-    const distance = iftarDate - now;
+setInterval(function(){
 
-    const days = Math.floor(distance / (1000*60*60*24));
-    const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-    const seconds = Math.floor((distance % (1000*60)) / 1000);
+const now = new Date().getTime();
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-}, 1000);
+const distance = iftarDate - now;
+
+const days=Math.floor(distance/(1000*60*60*24));
+const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+const seconds=Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML=days;
+document.getElementById("hours").innerHTML=hours;
+document.getElementById("minutes").innerHTML=minutes;
+document.getElementById("seconds").innerHTML=seconds;
+
+},1000);
